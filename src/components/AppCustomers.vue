097cfda1729e43c19b20bf6c577c2226cecb3d9c
boris7ran <boris.ran7@gmail.com>
2019-06-28 20:11:27 +0200

    Zadatak 4

--- src/components/AppCustomers.vue
+++ src/components/AppCustomers.vue
@@ -1,21 +1,23 @@
 <template>
     <div>
-        Text
         <ul>
             <li v-for="(customer, index) in customers" :key=index>
-                <SingleCustomer :customer="customer" @delete-customer="deleteCustomer"/>
+                <SingleCustomer :customer="customer" @delete-customer="deleteCustomer(index)"/>
             </li>
         </ul>
+        <CustomerForm />
     </div>
 </template>
 
 <script>
 import {customerService} from '@/services/CustomerService.js'
 import SingleCustomer from './SingleCustomer'
+import CustomerForm from './CustomerForm'
 
 export default {
     components: {
-        SingleCustomer
+        SingleCustomer,
+        CustomerForm
     },
 
     data () {
@@ -26,6 +28,7 @@ export default {
 
     methods: {
         deleteCustomer(index) {
+            console.log(index);
             customerService.remove (this.customers[index])
         }
     }
---
+++ src/components/CustomerForm.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <form @submit.prevent="handleAdd">
+            <div>
+                <label for="fullName">Full Name</label>
+                <input type="text" id="fullName" v-model="newCustomer.fullName" required>
+            </div>
+            <div>
+                <label for="email">Email</label>
+                <input type="email" id="email" v-model="newCustomer.email" required>
+            </div>
+
+            <div>
+                <button type="submit">Add Customer</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+import {customerService} from '@/services/CustomerService.js'
+
+export default {
+    data () {
+        return {
+            newCustomer: this.getDefaults()
+        }
+    },
+
+    methods: {
+        getDefaults () {
+            return {
+                fullName: '',
+                email: '',
+                products: []
+            }
+        },
+
+        handleAdd () {
+            customerService.add(this.newCustomer);
+            this.newCustomer = this.getDefaults;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>

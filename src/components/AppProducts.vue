68b6fa9d3dd07d16915ad2ca79efb01f943b1111
boris7ran <boris.ran7@gmail.com>
2019-06-28 21:17:10 +0200

    Zadatak 6 i 7

--- src/components/AppProducts.vue
+++ src/components/AppProducts.vue
@@ -1,8 +1,12 @@
 <template>
     <div>
+        <input type="text" v-model="search">
+
         <ul>
-            <li v-for="(product, index) in products" :key=index>
-                {{ product.id }}. {{ product.name }}
+            <li v-for="(product, index) in filteredProducts" :key=index>
+                {{ product.id }}. {{ product.name }} Quantity: {{ product.quantity}}
+                <button @click="handleIncrease(product)">+</button>
+                <button @click="handleDecrease(product)">-</button>
             </li>
         </ul>
     </div>
@@ -14,7 +18,24 @@ import {productService} from '@/services/ProductService'
 export default {
     data () {
         return {
-            products: productService.list()
+            products: productService.list(),
+            search: ''
+        }
+    },
+
+    methods: {
+        handleIncrease(product){
+            productService.increment(product);
+        },
+
+        handleDecrease(product){
+            productService.decrement(product);
+        }
+    },
+
+    computed: {
+        filteredProducts () {
+            return this.products.filter( product => product.name.toLowerCase().includes(this.search.toLowerCase()))
         }
     }
 }
